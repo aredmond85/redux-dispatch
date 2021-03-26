0a9baa4bdced62c48d9c859df5bbf2ecfb1dba97
Austin Redmond <aredmond85@gmail.com>
2021-03-26 14:53:55 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -9,14 +9,13 @@ function changeState(state, action){
     }
   }
 
-function dispatch(action){
-  state = changeState(state, action)
-  return state
+function render(){
+    document.body.textContent = state.count
 }
 
-dispatch({type: 'INCREASE_COUNT'})
-  // => {count: 1}
-dispatch({type: 'INCREASE_COUNT'})
-  // => {count: 2}
-dispatch({type: 'INCREASE_COUNT'})
-  // => {count: 3} 
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+
+render() 
